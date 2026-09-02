8d73b14a0f26850c9f211a1e262de61afcfc4a70
Maciej Witos <maciej.witos@gmail.com>
2026-09-02 13:53:24 +0200

    pusty interfejs

--- array.py
+++ array.py
@@ -1,3 +1,18 @@
+def printArray():
+    pass
+def entervalues():
+    pass
+def minvalue():
+    pass
+def maxvalue():
+    pass
+def calculateSum():
+    pass
+def avgvalue():
+    pass
+def menu():
+    pass
+
 def main():
     print(f'Test, {__name__}')
 
